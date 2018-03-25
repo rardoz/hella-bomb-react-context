dd56d38db491775076df45d2beee5e68cab2721e
Robert Greene <robert@zardozcs.com>
2018-03-25 02:27:38 -0700

    Sassy it up

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-# hella-bomb-react-context-from-the-future
-This is a React 16.3 Context API component that greatly reduces the complexity of handling global state. ✌Duces Redux! Finally!!! Clean, simple, and lean state management.
+# 💣 💥 Hella Bomb React Context 16.3.0-alpha.x 💥  💣
 
+[![npm version](https://badge.fury.io/js/hella-bomb-react-context.svg)](https://badge.fury.io/js/hella-bomb-react-context)
+[![dependencies](https://david-dm.org/username/repo.svgbadge?sanitize=true)](https://github.com/rardoz/hella-bomb-react-context/blob/master/package.json)
+[![coverage](https://raw.githubusercontent.com/rardoz/hella-bomb-react-context/master/badges/coverage.svg?sanitize=true)](https://circleci.com/gh/rardoz/hella-bomb-react-context)
+[![test](https://circleci.com/gh/rardoz/hella-bomb-react-context.svg?style=svg)](https://circleci.com/gh/rardoz/hella-bomb-react-context)
 
-## Example usage
+This is a [React 16.3 Context API](https://github.com/reactjs/rfcs/blob/master/text/0002-new-version-of-context.md) component that greatly reduces the complexity of handling global state. ✌Duces Redux! Finally!!! Clean, simple, and lean state management.
 
-Wrap your application in a StateProvider component. You can extend the StateProvider to setup your own initial state and custom actions.
+## 🧠 Example usage
+
+Wrap your application in a `StateProvider` component. You can extend the `StateProvider` to setup your own initial state and custom actions.
 
 ```jsx
 import { StateProvider } from 'hella-bomb-react-context'
@@ -21,7 +26,7 @@ export default class extends React.Component {
 }
 ```
 
-All of the child components now have access to the global state. You can set state and read state on the fly by calling actions.setState within a Consumer component as seen below.
+All of the child components now have access to the global state. You can set state and read state by calling actions.setState within a `Consumer` component as seen below.
 
 ```jsx
 import { Consumer } from 'hella-bomb-react-context'
@@ -37,3 +42,27 @@ export default () => (
   </Consumer>
 )
 ```
+
+
+## 🧞 Contribution Guidelines
+
+Feel free to open a PR! Please update/add tests.
+
+## 🦖 Development
+
+* Pull down the latest from github.
+* Run `npm install`
+* Modify the /src/index.js file
+* Run tests `npm test`
+
+## 🌈 Support me 
+
+📠 [Linkedin](https://linkedin.com/in/rardoz)
+
+📸 [Instagram](https://instagram.com/rardoz)
+
+👽 [Zardoz CS](http://zardozcs.com)
+
+🧙 [Spellcaster](http://spellcstr.com/app/)
+
+👨‍🎤 [neon.computer](http://neon.computer)
--- build/index.js
+++ build/index.js
@@ -1,1 +1,88 @@
-console.log(123);
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.StateProvider = exports.Consumer = exports.Provider = undefined;
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+// Initialize a context
+var Context = (0, _react.createContext)("hbrc-global-context");
+
+// This context contains two interesting components
+var Provider = Context.Provider,
+    Consumer = Context.Consumer;
+exports.Provider = Provider;
+exports.Consumer = Consumer;
+
+var StateProvider = exports.StateProvider = function (_Component) {
+  _inherits(StateProvider, _Component);
+
+  function StateProvider() {
+    var _ref;
+
+    var _temp, _this, _ret;
+
+    _classCallCheck(this, StateProvider);
+
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = StateProvider.__proto__ || Object.getPrototypeOf(StateProvider)).call.apply(_ref, [this].concat(args))), _this), _this.state = {
+      //initial state goes here
+    }, _this.addToState = function () {
+      var newState = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+      _this.setState(Object.assign({}, newState));
+    }, _temp), _possibleConstructorReturn(_this, _ret);
+  }
+
+  _createClass(StateProvider, [{
+    key: "render",
+    value: function () {
+      function render() {
+        var _this2 = this;
+
+        return _react2["default"].createElement(
+          Provider,
+          {
+            value: {
+              state: this.state,
+              actions: {
+                setState: function () {
+                  function setState() {
+                    var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+                    return _this2.addToState(state);
+                  }
+
+                  return setState;
+                }()
+              }
+            }
+          },
+          this.props.children
+        );
+      }
+
+      return render;
+    }()
+  }]);
+
+  return StateProvider;
+}(_react.Component);
+
+StateProvider.defaultProps = {
+  children: []
+};
